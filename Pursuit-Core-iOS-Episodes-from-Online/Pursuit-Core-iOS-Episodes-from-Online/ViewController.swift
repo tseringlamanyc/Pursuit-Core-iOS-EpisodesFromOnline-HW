d5647b103f645ffdd5e05d0c74cd86c43c422572
Ben Stone <benjamin.h.stone@gmail.com>
2019-09-05 13:09:24 -0400

    add new project

---
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pursuit-Core-iOS-Episodes-from-Online
+//
+//  Created by Benjamin Stone on 9/5/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-OnlineTests/Pursuit_Core_iOS_Episodes_from_OnlineTests.swift
@@ -0,0 +1,34 @@
+//
+//  Pursuit_Core_iOS_Episodes_from_OnlineTests.swift
+//  Pursuit-Core-iOS-Episodes-from-OnlineTests
+//
+//  Created by Benjamin Stone on 9/5/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import XCTest
+@testable import Pursuit_Core_iOS_Episodes_from_Online
+
+class Pursuit_Core_iOS_Episodes_from_OnlineTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
