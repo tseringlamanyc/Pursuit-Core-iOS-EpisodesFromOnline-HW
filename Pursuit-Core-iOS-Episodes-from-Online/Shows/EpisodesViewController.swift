e4cb29a24d79351a433fa9f7f7fcf5ffe2b93cd1
Tsering Lama <tseringlama@pursuit.org>
2019-12-11 16:11:50 -0500

    made swift files for episodes

---
+++ Pursuit-Core-iOS-Episodes-from-Online/Shows/EpisodesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EpisodesViewController.swift
+//  Pursuit-Core-iOS-Episodes-from-Online
+//
+//  Created by Tsering Lama on 12/11/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class EpisodesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
